958743c7169592d3364a1a3bf2133c3f6d16585a
ramouslipe <filiperamosps@gmail.com>
2026-04-08 17:02:40 -0300

    feat: cria entidade de usuário closes #7

---
+++ backend/src/entities/User.ts
@@ -0,0 +1,40 @@
+import { 
+  Entity, 
+  PrimaryGeneratedColumn, 
+  Column, 
+  CreateDateColumn, 
+  UpdateDateColumn 
+} from "typeorm";
+
+export enum UserRole {
+  ALUNO = "aluno",
+  EMPRESA = "empresa"
+}
+
+@Entity("users")
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  id!: string; // O '!' resolve o erro de inicialização
+
+  @Column({ type: "varchar", length: 100 })
+  name!: string;
+
+  @Column({ type: "varchar", unique: true })
+  email!: string;
+
+  @Column({ type: "varchar", select: false })
+  password!: string;
+
+  @Column({
+    type: "enum",
+    enum: UserRole,
+    default: UserRole.ALUNO
+  })
+  role!: UserRole;
+
+  @CreateDateColumn()
+  created_at!: Date;
+
+  @UpdateDateColumn()
+  updated_at!: Date;
+}
